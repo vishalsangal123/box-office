3645081e278197007c948f74943db99afe568b06
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-22 00:38:21 +0530

    learn about rendering

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
+import { Switch ,Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+    This is home page
+    </Route>
+    <Route exact path="/started">
+    This is started
+    </Route>
+    <Route>
+      This is 404 page
+    </Route>
+    </Switch>
   );
 }
 
