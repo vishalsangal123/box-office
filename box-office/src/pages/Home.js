7a20d9029f8e9df1e3f44e691e0548e29af91319
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-22 22:46:47 +0530

    added input and fetch result

--- box-office/src/pages/Home.js
+++ box-office/src/pages/Home.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React , {useState} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>this is home</MainPageLayout>;
+  const [input,setInput]=useState(' ');
+  
+  const onInputChange=(ev)=>{
+
+    setInput(ev.target.value);
+  //  console.log(ev.target.value);
+
+  };
+  
+  const onSearch = () => {
+   // https://api.tvmaze.com/search/shows?q=men
+   fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+    console.log(result);
+  });
+};
+  
+const onKeyDown=(ev)=>{
+  if(ev.keyCode===13)
+  onSearch()
+//console.log(ev.keyCode);
+}
+
+  return( 
+     <MainPageLayout>
+    
+        <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+        <button type="button" onClick={onSearch}>Search</button>
+      
+    </MainPageLayout>
+  );
 };
 
 export default Home;
