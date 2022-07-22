83e4ba21954be0e2a551e747bbe8b7f6a80807aa
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-22 23:19:13 +0530

    added search option

--- box-office/src/pages/Home.js
+++ box-office/src/pages/Home.js
@@ -1,62 +1,99 @@
-import React , {useState} from 'react';
-import MainPageLayout from '../components/MainPageLayout';
-import { apiGet } from '../misc/config';
 
-const Home = () => {
-  const [input,setInput]=useState(' ');
-  const[results,setResults]=useState(null);
-  const onInputChange=(ev)=>{
 
-    setInput(ev.target.value);
-  //  console.log(ev.target.value);
-
-  };
-  
+  //  console.log(ev.target.value);  
 //   const onSearch = () => {
 //    // https://api.tvmaze.com/search/shows?q=men
 //    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
 //     setResults(result);
 //     console.log(result);
 //   });
-// };
-  
-const onSearch = () => {
-  apiGet(`/search/shows?q=${input}`).then(result => {
-    setResults(result);
-  });
-};
+// }
 
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
-const onKeyDown=(ev)=>{
-  if(ev.keyCode===13)
-  onSearch()
-//console.log(ev.keyCode);
-}
+const Home = () => {
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
 
-const renderResults = () => {
-  if (results && results.length === 0) {
-    return <div>No results</div>;
-  }
+  const isShowsSearch = searchOption === 'shows';
 
-  if (results && results.length > 0) {
-    return (
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
       <div>
-        {results.map(item => (
-          <div key={item.show.id}>{item.show.name}</div>
-        ))}
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
       </div>
-    );
-  }
 
-  return null;
-};
-
-  return( 
-     <MainPageLayout>
-    
-        <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
-        <button type="button" onClick={onSearch}>Search</button>
-        {renderResults()}
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
