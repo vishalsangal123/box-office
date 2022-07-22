04b683d625d1361277519b27cb2758632d2f164d
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-22 23:05:41 +0530

    displayed show results and small fetch refactor

--- box-office/src/pages/Home.js
+++ box-office/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React , {useState} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input,setInput]=useState(' ');
-  
+  const[results,setResults]=useState(null);
   const onInputChange=(ev)=>{
 
     setInput(ev.target.value);
@@ -11,25 +12,51 @@ const Home = () => {
 
   };
   
-  const onSearch = () => {
-   // https://api.tvmaze.com/search/shows?q=men
-   fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
-    console.log(result);
+//   const onSearch = () => {
+//    // https://api.tvmaze.com/search/shows?q=men
+//    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+//     setResults(result);
+//     console.log(result);
+//   });
+// };
+  
+const onSearch = () => {
+  apiGet(`/search/shows?q=${input}`).then(result => {
+    setResults(result);
   });
 };
-  
+
+
 const onKeyDown=(ev)=>{
   if(ev.keyCode===13)
   onSearch()
 //console.log(ev.keyCode);
 }
 
+const renderResults = () => {
+  if (results && results.length === 0) {
+    return <div>No results</div>;
+  }
+
+  if (results && results.length > 0) {
+    return (
+      <div>
+        {results.map(item => (
+          <div key={item.show.id}>{item.show.name}</div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
   return( 
      <MainPageLayout>
     
         <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
         <button type="button" onClick={onSearch}>Search</button>
-      
+        {renderResults()}
     </MainPageLayout>
   );
 };
