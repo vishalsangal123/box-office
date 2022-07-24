15cfd604733d1422766089b5e6b386c7b341b2bf
Vishal Sangal <vishalsangal40@gmail.com>
2022-07-24 18:42:27 +0530

    created show page component

--- box-office/src/pages/Show.js
+++ box-office/src/pages/Show.js
@@ -1,82 +1,99 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 const Show = () => {
   const { id } = useParams();
 
-const initialState={
-  isLoading:true,
-  error:null,
-  show:null
-
-}
-// action is an object of type {
-// type:'d'  // in type written which type of action is performed
-// }
-const reducer=(prevState,action)=>{
-  switch(action.type)
-  {
-    
-   case 'FETCH_SUCCESS':{
-    return {...prevState,isLoading:false,error:null,show:action.show};
-   }
-   case 'FETCH_FAILED':{
-    return {...prevState,isLoading:false,error:action.error};
-   }
-  default:return prevState;
-
-  }
-}
-
-  const [{show,isLoading,error},dispatch]=useReducer(reducer,initialState);
-  // const [show, setShow] = useState(null);
-  // const [isLoading,setIsLoading]=useState(true);
-  // const [error,setError]=useState(null);
-
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
+    let isMounted = true;
 
-        let isMounted=true;
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      // setTimeout(()=>{
-        if(isMounted)
-        {
-          dispatch({type:'FETCH_SUCCESS',show:results})
-        // setShow(results);
-        // setIsLoading(false);
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
         }
-      // },2000);
-    }).catch(err=>{
-         if(isMounted)
-         {        
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
 
-          dispatch({type:'FETCH_FAILED',error:err.message})
-          //  setError(err.message);
-          //  setIsLoading(false);
-    
-        }
-            });
-    return ()=>{
-      isMounted=false;
-    }
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
-  
-   console.log('show', show);
-
-  if(isLoading)
-  {
-    return <div>Data is being loaded..</div>
-  }
-  if(error)
-  {
-    return <div>ERROR OCCURED:{error} </div>
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
   }
 
- 
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
 
-  return <div>this is show page</div>;
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
